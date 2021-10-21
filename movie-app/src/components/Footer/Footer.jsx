0042064b08a72f52399308e1119bd3511ce2fed4
Sergey-Chernushevich <sergeychernushevich@gmail.com>
2021-10-22 00:51:31 +0300

    fix links

--- movie-app/src/components/Footer/Footer.jsx
+++ movie-app/src/components/Footer/Footer.jsx
@@ -5,9 +5,12 @@ function Footer() {
   return (
     <div className={styles.footerWrapper}>
       <p>
-        Developed by <a href="#">Sergey Chernushevich</a>, <b>!!!!ICON</b> Using
-        React JS & Redux JS integrated with external module data API{" "}
-        <a href="#">OMDB</a>
+        Developed by{" "}
+        <a href="https://github.com/Sergey-Chernushevich">
+          Sergey Chernushevich
+        </a>
+        , <b>!!!!ICON</b> Using React JS & Redux JS integrated with external
+        module data API <a href="http://www.omdbapi.com/">OMDB</a>
       </p>
     </div>
   );
